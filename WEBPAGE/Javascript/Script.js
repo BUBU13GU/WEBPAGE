a3aef99e41c6010b1fb91ca88082fad3132340fd
BUBU13GU <71035245+BUBU13GU@users.noreply.github.com>
2023-07-04 20:07:51 +0200

    Add files via upload

---
+++ WEBPAGE/Javascript/Script.js
@@ -0,0 +1,126 @@
+var final = 0;
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";}
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";}
+
+    function button1() {
+        var amount = 3800
+        
+        var x = prompt("Enter how many products you want: ")
+        if (isNaN(x)) 
+        {
+            alert("Please Enter a Number.")
+        }else {
+        var y = x * amount
+        final =+ y
+        alert("Your Amount = R" + final)
+        window.location = '../Html/Checkout.html'
+        }
+    }
+
+    function button2() {
+        var amount = 4200
+        
+        var x = prompt("Enter how many products you want: ")
+        if (isNaN(x)) 
+        {
+            alert("Please Enter a Number.")
+        }else {
+        var y = x * amount
+        final =+ y
+        alert("Your Amount = R" + final)
+        window.location = '../Html/Checkout.html'
+        }
+    }
+    function button3() {
+        var amount = 3000
+        
+        var x = prompt("Enter how many products you want: ")
+        if (isNaN(x)) 
+        {
+            alert("Please Enter a Number.")
+        }else {
+        var y = x * amount
+        final =+ y
+        alert("Your Amount = R" + final)
+        window.location = '../Html/Checkout.html'
+        }
+    }
+    function button4() {
+        var amount = 3500
+        
+        var x = prompt("Enter how many products you want: ")
+        if (isNaN(x)) 
+        {
+            alert("Please Enter a Number.")
+        }else {
+        var y = x * amount
+        final =+ y
+        alert("Your Amount = R" + final)
+        window.location = '../Html/Checkout.html'
+        }
+    }
+    function button5() {
+        var amount = 4000
+        
+        var x = prompt("Enter how many products you want: ")
+        if (isNaN(x)) 
+        {
+            alert("Please Enter a Number.")
+        }else {
+        var y = x * amount
+        final =+ y
+        alert("Your Amount = R" + final)
+        window.location = '../Html/Checkout.html'
+        }
+    }
+    function button6() {
+        var amount = 3850
+        
+        var x = prompt("Enter how many products you want: ")
+        if (isNaN(x)) 
+        {
+            alert("Please Enter a Number.")
+        }else {
+        var y = x * amount
+        final =+ y
+        alert("Your Amount = R" + final)
+        window.location = '../Html/Checkout.html'
+        }
+    }
+    function button7() {
+        var amount = 2775
+        
+        var x = prompt("Enter how many products you want: ")
+        if (isNaN(x)) 
+        {
+            alert("Please Enter a Number.")
+        }else {
+        var y = x * amount
+        final =+ y
+        alert("Your Amount = R" + final)
+        window.location = '../Html/Checkout.html'
+        }
+    }
+    function button8() {
+        var amount = 3950
+        
+        var x = prompt("Enter how many products you want: ")
+        if (isNaN(x)) 
+        {
+            alert("Please Enter a Number.")
+        }else {
+        var y = x * amount
+        final =+ y
+        alert("Your Amount = R" + final)
+        window.location = '../Html/Checkout.html'
+        }
+    }
+
+    function thanks() {
+        alert("Thank you for joining our news letter!")
+    }
